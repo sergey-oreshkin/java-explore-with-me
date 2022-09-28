bc593c2190797a772f109e44f6d156a9f056f5b7
sergey-oreshkin <joknife79@ya.ru>
2022-09-28 14:41:15 +0500

    feat: log errorDto

--- ewm/src/main/java/ru/practicum/explorewithme/exception/ControllerExceptionHandler.java
+++ ewm/src/main/java/ru/practicum/explorewithme/exception/ControllerExceptionHandler.java
@@ -21,105 +21,112 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorDto handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-        log.error(ex.getMessage());
-        return ErrorDto.builder()
+        ErrorDto errorDto = ErrorDto.builder()
                 .errors(Collections.emptyList())
                 .message(ex.getFieldError().getDefaultMessage())
                 .reason(String.valueOf(ex.getFieldError().getRejectedValue()))
                 .status(HttpStatus.BAD_REQUEST)
                 .timestamp(LocalDateTime.now())
                 .build();
+        log.error(errorDto.toString());
+        return errorDto;
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorDto handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
-        log.error(ex.getMessage());
-        return ErrorDto.builder()
+        ErrorDto errorDto = ErrorDto.builder()
                 .errors(Collections.emptyList())
                 .message(ex.getMessage())
                 .reason(ex.getErrorCode())
                 .status(HttpStatus.BAD_REQUEST)
                 .timestamp(LocalDateTime.now())
                 .build();
-
+        log.error(errorDto.toString());
+return errorDto;
     }
 
     @ExceptionHandler(ConflictException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorDto handleConflictException(ConflictException ex) {
-        log.error(ex.getMessage());
-        return ErrorDto.builder()
+        ErrorDto errorDto = ErrorDto.builder()
                 .errors(Collections.emptyList())
                 .message(ex.getMessage())
                 .reason(ex.getRejectedValue())
                 .status(HttpStatus.CONFLICT)
                 .timestamp(LocalDateTime.now())
                 .build();
+        log.error(errorDto.toString());
+        return errorDto;
     }
 
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorDto handleNotFoundException(NotFoundException ex) {
-        log.error(ex.getMessage());
-        return ErrorDto.builder()
+        ErrorDto errorDto = ErrorDto.builder()
                 .errors(Collections.emptyList())
                 .message(ex.getMessage())
                 .reason(ex.getRejectedValue())
                 .status(HttpStatus.NOT_FOUND)
                 .timestamp(LocalDateTime.now())
                 .build();
+        log.error(errorDto.toString());
+        return errorDto;
     }
 
     @ExceptionHandler(ValidationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorDto handleValidationException(ValidationException ex) {
-        log.error(ex.getMessage());
-        return ErrorDto.builder()
+        ErrorDto errorDto = ErrorDto.builder()
                 .errors(Collections.emptyList())
                 .message(ex.getMessage())
                 .reason(ex.getRejectedValue())
                 .status(HttpStatus.BAD_REQUEST)
                 .timestamp(LocalDateTime.now())
                 .build();
+        log.error(errorDto.toString());
+        return errorDto;
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorDto handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
-        log.error(ex.getMessage());
-        return ErrorDto.builder()
+        ErrorDto errorDto = ErrorDto.builder()
                 .errors(Collections.emptyList())
                 .message(ex.getMessage())
                 .reason("")
                 .status(HttpStatus.BAD_REQUEST)
                 .timestamp(LocalDateTime.now())
                 .build();
+        log.error(errorDto.toString());
+        return errorDto;
     }
 
     @ExceptionHandler(InvalidDataAccessApiUsageException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorDto handleUnknownException(InvalidDataAccessApiUsageException ex) {
-        log.error(ex.getMessage());
-        return ErrorDto.builder()
+        ErrorDto errorDto = ErrorDto.builder()
                 .errors(Collections.emptyList())
                 .message(ex.getMessage())
                 .reason("")
                 .status(HttpStatus.BAD_REQUEST)
                 .timestamp(LocalDateTime.now())
                 .build();
+        log.error(errorDto.toString());
+        return errorDto;
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorDto handleUnknownException(Exception ex) {
-        log.error(ex.getMessage());
-        return ErrorDto.builder()
+        ErrorDto errorDto =  ErrorDto.builder()
                 .errors(Collections.emptyList())
                 .message(ex.getMessage())
                 .reason("")
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .timestamp(LocalDateTime.now())
                 .build();
+        log.error(errorDto.toString());
+        return errorDto;
     }
 }
