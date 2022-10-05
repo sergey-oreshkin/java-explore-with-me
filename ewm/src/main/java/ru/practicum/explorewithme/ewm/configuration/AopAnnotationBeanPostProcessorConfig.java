dd99c68d4a6c2d46cdad5d33ff9353171a0182d3
sergey-oreshkin <joknife79@ya.ru>
2022-10-05 13:52:17 +0500

    feat: check method parameters for ControllerLog annotation

---
+++ ewm/src/main/java/ru/practicum/explorewithme/ewm/configuration/AopAnnotationBeanPostProcessorConfig.java
@@ -0,0 +1,14 @@
+package ru.practicum.explorewithme.ewm.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.practicum.explorewithme.ewm.aop.AopAnnotationBeanPostProcessor;
+
+@Configuration
+public class AopAnnotationBeanPostProcessorConfig {
+
+    @Bean
+    public AopAnnotationBeanPostProcessor aopAnnotationBeanPostProcessor(){
+        return new AopAnnotationBeanPostProcessor();
+    }
+}
